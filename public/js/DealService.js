b047ea6b2b2826803fedffa8ec05ad287b0600ca
Nb <nael.sylvain@gmail.com>
2015-03-15 21:19:33 +0100

    Découpage des fichiers

---
+++ public/js/DealService.js
@@ -0,0 +1,11 @@
+var module = angular.module('DealServices', []);
+
+module.service('DealsService', ['$http', function($http){
+
+	this.getDeals = function(type, top, skip, callback){
+		$http.get('/deals/'+type+'?$top='+top+'&$skip='+skip).success(function(data, status, headers, config){
+          callback(data);
+        })
+	};
+
+}]);
